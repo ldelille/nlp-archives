e4ce3611e8e11e20f81e8864a64d0744155948d7
Delille Louis <louis.delille@supelec.fr>
2021-01-10 17:05:12 +0100

    refactor LDA class

--- pipelines/lda.py
+++ pipelines/lda.py
@@ -1,23 +1,34 @@
 import pickle
 
 from gensim import corpora
+import pandas as pd
+from pipelines.preprocessing import ArticlePreprocessor
 
 
 class LDA():
-    def __init__(self, title_tokens, text_tokens):
-        self.title_tokens = title_tokens
-        self.text_tokens = text_tokens
+    def __init__(self, preprocessor: ArticlePreprocessor):
+        self.preprocessor = preprocessor
+        self.dic_title
+        self.dic_text
+        self.corpus_title
+        self.corpus_text
+
+    def launchLDA(self):
+        dic_title = corpora.Dictionary(self.preprocessor.title_tokens)
+        dic_text = corpora.Dictionary(self.preprocessor.text_tokens)
+        corpus_title = [dic_title.doc2bow(token) for token in self.preprocessor.title_tokens]
+        corpus_text = [dic_text.doc2bow(token) for token in self.preprocessor.text_tokens]
+
+        pickle.dump(corpus_title, open('corpus_title.pkl', 'wb'))
+        dic_title.save('dic_title.gensim')
+        pickle.dump(corpus_title, open('corpus_text.pkl', 'wb'))
+        dic_text.save('dic_text.gensim')
 
 
 def main():
+    news_df = pd.read_csv("./articles.csv")
+    article_preprocessor = ArticlePreprocessor(news_df)
+    article_preprocessor.fully_preprocess()
+    lda = LDA(article_preprocessor)
+    lda.launchLDA()
 
-
-    dic_title = corpora.Dictionary(title_tokens)
-    dic_text = corpora.Dictionary(text_tokens)
-    corpus_title = [dic_title.doc2bow(token) for token in title_tokens]
-    corpus_text = [dic_text.doc2bow(token) for token in text_tokens]
-
-    pickle.dump(corpus_title, open('corpus_title.pkl', 'wb'))
-    dic_title.save('dic_title.gensim')
-    pickle.dump(corpus_title, open('corpus_text.pkl', 'wb'))
-    dic_text.save('dic_text.gensim')
