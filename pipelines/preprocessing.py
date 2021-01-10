8b932c8819691aca1d72d19ae6cdc9d61e20811f
Delille Louis <louis.delille@supelec.fr>
2021-01-10 16:54:41 +0100

    refactor PreProcessor class

--- pipelines/preprocessing.py
+++ pipelines/preprocessing.py
@@ -24,61 +24,55 @@ my_fr_stop = fr_stop.union({'ce', 'celui', 'cette', 'cet', 'celui-là', 'celui-c
 
 nlp = spacy.load("fr_core_news_sm")
 
-def prepare_text(text):
-    """
-    Input:
-    ------
-    text: string, raw text
-
-    Output:
-    ------
-    tokens: list of string, tokenized, filtered and lemmatized words from the input text
-    """
-    tokens = tokenize(text)  # split and lower case
-    tokens = [re.sub(r'\b\d+\b', '', token) for token in tokens]  # get rid of digits
-    tokens = [token for token in tokens if len(token) > 4]  # arbitrary length, +get rid of empty strings
-    tokens = [token for token in tokens if token not in my_fr_stop]  # stopwords
-    doc = nlp(' '.join(tokens))  # pave the wave for spacy lemmatizer
-    tokens = [token.lemma_ for token in doc]  # obtain lemmas
-    return tokens
-
-
-def tokenize(text):
-    lda_tokens = []
-    tokens = parser(text)
-    for token in tokens:
-        if token.orth_.isspace():
-            continue
-        elif token.like_url:
-            lda_tokens.append('URL')
-        elif token.orth_.startswith('@'):
-            lda_tokens.append('SCREEN_NAME')
-        else:
-            lda_tokens.append(token.lower_)
-    return lda_tokens
 
 
 class ArticlePreprocessor:
-    def __init__(self, article: str):
-        self.article = article
+    def __init__(self, article_df):
+        self.article_df = article_df
+        self.title_tokens = []
+        self.text_tokens = []
+
+    def fully_preprocess(self):
+        for t in self.article_df.title:
+            tokens = prepare_text(t)
+            self.title_tokens.append(tokens)
+        for t in self.article_df.text:
+            tokens = prepare_text(t)
+            self.text_tokens.append(tokens)
+
+    def prepare_text(self, text):
+        """
+        Input:
+        ------
+        text: string, raw text
+
+        Output:
+        ------
+        tokens: list of string, tokenized, filtered and lemmatized words from the input text
+        """
+        tokens = tokenize(text)  # split and lower case
+        tokens = [re.sub(r'\b\d+\b', '', token) for token in tokens]  # get rid of digits
+        tokens = [token for token in tokens if len(token) > 4]  # arbitrary length, +get rid of empty strings
+        tokens = [token for token in tokens if token not in my_fr_stop]  # stopwords
+        doc = nlp(' '.join(tokens))  # pave the wave for spacy lemmatizer
+        tokens = [token.lemma_ for token in doc]  # obtain lemmas
+        return tokens
+
+    def tokenize(text):
+        lda_tokens = []
+        tokens = parser(text)
+        for token in tokens:
+            if token.orth_.isspace():
+                continue
+            elif token.like_url:
+                lda_tokens.append('URL')
+            elif token.orth_.startswith('@'):
+                lda_tokens.append('SCREEN_NAME')
+            else:
+                lda_tokens.append(token.lower_)
+        return lda_tokens
 
 
 def main():
     news_df = pd.read_csv("./articles.csv")
-    title_tokens = []
-    text_tokens = []
-    ## Apply on titles ##
-    for t in news_df.title:
-        tokens = prepare_text(t)
-        title_tokens.append(tokens)
-
-    ## Apply on titles ##
-
-    for t in news_df.text:
-        tokens = prepare_text(t)
-        text_tokens.append(tokens)
-
-    print(news_df.title[:1])
-    print(title_tokens[:1])
-    print(news_df.text[:1][:20])
-    print(text_tokens[0][:10])
+    article_preprocessor = ArticlePreprocessor(news_df)
