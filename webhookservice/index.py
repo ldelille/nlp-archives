f8f2a60bc243e615a7db52291b29b90c63d7cf37
Delille Louis <louis.delille@supelec.fr>
2021-01-18 14:53:15 +0100

    Fixed webhook call and added example response

--- webhookservice/index.py
+++ webhookservice/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response
 import os
 import dialogflow
 import requests
@@ -11,41 +11,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/webhook', methods=['POST'])
+# function for responses
+def results():
+    # build a request object
+    req = request.get_json(force=True)
+
+    # fetch action from json
+    action = req.get('queryResult').get('action')
+
+    # return a fulfillment response
+    return {'fulfillmentText': 'This is a response from webhook.'}
+
+# create a route for webhook
+@app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    data = request.get_json(silent=True)
-    if data['queryResult']['queryText'] == 'yes':
-        reply = {
-            "fulfillmentText": "Ok. Tickets booked successfully.",
-        }
-        return jsonify(reply)
-
-    elif data['queryResult']['queryText'] == 'no':
-        reply = {
-            "fulfillmentText": "Ok. Booking cancelled.",
-        }
-        return jsonify(reply)
-
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    message = request.form['message']
-    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-    fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
-    response_text = { "message":  fulfillment_text }
-    return jsonify(response_text)
-
-
-def detect_intent_texts(project_id, session_id, text, language_code):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    if text:
-        text_input = dialogflow.types.TextInput(
-            text=text, language_code=language_code)
-        query_input = dialogflow.types.QueryInput(text=text_input)
-        response = session_client.detect_intent(
-            session=session, query_input=query_input)
-        return response.query_result.fulfillment_text
+    # return response
+    return make_response(jsonify(results()))
 
 if __name__ == "__main__":
     app.run()
