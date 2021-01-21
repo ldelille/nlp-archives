15da1a34ca72560bd85fd3723d3af67224b845ba
Delille Louis <louis.delille@supelec.fr>
2021-01-21 10:46:54 +0100

    refactor index flask

--- webhookservice/index.py
+++ webhookservice/index.py
@@ -11,18 +11,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# function for responses
 def results():
-    # build a request object
     req = request.get_json(force=True)
 
-    # fetch action from json
     action = req.get('queryResult').get('action')
 
-    # return a fulfillment response
     return {'fulfillmentText': 'This is a response from webhook.'}
 
-# create a route for webhook
+# route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     # return response
