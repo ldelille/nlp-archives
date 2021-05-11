7dfdb0f2c3c60bb09fa00abda4d42d5baeb2ae3b
Delille Louis <louis.delille@supelec.fr>
2021-05-11 20:46:26 +0200

    comply with robots.txt rules

--- scraping_service/ScrapingNews/settings.py
+++ scraping_service/ScrapingNews/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'ScrapingNews.spiders'
 #USER_AGENT = 'ScrapingNews (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
